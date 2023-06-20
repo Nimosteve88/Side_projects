5e098e6208dd2ab580b514ba2d6f366d0c9fa303
Nimosteve88 <85235717+Nimosteve88@users.noreply.github.com>
2023-06-20 22:57:44 +0100

    Update app.py

--- weather_forecast_webapp/app.py
+++ weather_forecast_webapp/app.py
@@ -33,7 +33,9 @@ class Weather_info(MethodView):
         # You can replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
         weather = Weather(apikey=api, city=city_name, lat=lat, lon=lon)
         weather_info = weather.next_12h_simplified()
-        return render_template('weather.html', weather_info=weather_info)
+        weather_icon = weather.getweathericon()
+        print(weather_icon)
+        return render_template('weather.html', weather_info=weather_info, weather_icon=weather_icon)
 
 price = []
 
